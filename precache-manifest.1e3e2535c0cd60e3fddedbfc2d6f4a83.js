70c99405f257bab8d900f532ed6ac2738ed72625
Alex Glover <alex@circulation.com>
2020-05-15 16:51:16 -0400

    Updates

---
+++ precache-manifest.1e3e2535c0cd60e3fddedbfc2d6f4a83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d14fe8cd4dfaad5c78e893ae19fcdda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01abf9623f0b0dcd87b7",
+    "url": "/static/css/main.bb86e0e3.chunk.css"
+  },
+  {
+    "revision": "544a3538989d659154b4",
+    "url": "/static/js/2.621650db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.621650db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01abf9623f0b0dcd87b7",
+    "url": "/static/js/main.6334805f.chunk.js"
+  },
+  {
+    "revision": "0a1be12192fd86b0ff12",
+    "url": "/static/js/runtime-main.6bc68732.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
